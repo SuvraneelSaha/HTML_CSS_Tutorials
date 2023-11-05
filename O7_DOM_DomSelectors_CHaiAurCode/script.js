6f4c61329334019fb6621fc5e24cd27f197b7555
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-11-05 22:45:05 +0530

    dom selectors nodelist and html collection

---
+++ O7_DOM_DomSelectors_CHaiAurCode/script.js
@@ -0,0 +1,202 @@
+document.getElementById('title');
+
+// selects the title element
+
+document.getElementById('title').id;
+
+// o/p 'title'
+
+document.getElementById('title').class
+
+// undefined cause we there is no function like that 
+
+document.getElementById('title').className
+
+// 'heading'
+
+
+document.getElementById('title').getAttribute
+
+// error showing that getAttribute is a function 
+
+document.getElementById('title').getAttribute()
+
+// error showing at least 1 parameter is needed in the getAttribute() function 
+
+
+document.getElementById('title').getAttribute('id')
+
+// o/p 'title'
+
+document.getElementById('title').getAttribute('class')
+
+// o/p class name ie 'heading'
+
+// so in case of getAttribute() we need to give the key to the function as a parameter 
+// and then it will return the value of the key , here the keys are class and id 
+
+document.getElementById('title').setAttribute('class' , 'test');
+
+// this will override the original class attribute and set it to test 
+
+// this is a mistake normally done by all and you will also fall pray to it so dont worry 
+
+document.getElementById('title').setAttribute('class' , 'test heading');
+
+// this will additionally add a heading to the class attribute 
+//  ie the class attribute will have test as a class name 
+
+
+
+
+// from the console 
+
+document.getElementById("title");
+
+
+//  <h1 id=​"title" class=​"heading">​Dom Learning on chai aur code​</h1>​ 
+
+
+const title =  document.getElementById("title");
+
+// undefined
+
+title.style.backgroundColor = "red";
+
+// 'red'
+
+title.style.padding = "10px";
+
+// '10px'
+
+title.style.borderRadius = "15px";
+
+// '15px'
+
+// Difference Between document.innerHTML , innerText , textContent
+
+title.textContent
+
+// 'Dom Learning on chai aur code test code '
+
+title.innerText
+
+// 'Dom Learning on chai aur code'
+
+title.innerHTML;
+
+// 'Dom Learning on chai aur code <span style="display: none;">test code </span>'
+
+
+document.getElementsByClassName("heading")
+
+// HTMLCollection [h1#title.heading, title: h1#title.heading]
+
+const test =  document.querySelector("h2")
+undefined
+test.innerHTML
+'Lorem ipsum dolor sit.'
+test.innerHTML = "NIGGA"
+'NIGGA'
+test.innerText = "WiGGA"
+'WiGGA'
+test.textContent = "WiGGAChan"
+'WiGGAChan'
+
+
+document.querySelector(".heading")
+
+//  <h1 id=​"title" class=​"heading">​…​</h1>​
+
+
+document.querySelector("#title") 
+
+// <h1 id=​"title" class=​"heading">​…​</h1>​
+
+
+document.querySelector('input[type="password"]')
+
+/* <input type=​"password" name id>​ */
+
+// all  CSS selectors can be used in it
+
+document.querySelector('ul')
+
+{/* <ul>​…​</ul> */}
+​
+const myul =  document.querySelector('ul')
+
+// undefined
+
+myul.querySelector('li')
+
+{/* <li>​…​</li>​ */}
+
+// as it is the first element 
+// undefined
+
+const turnGreen =  myul.querySelector('li')
+
+// undefined
+
+turnGreen.style.backgroundColor = "blue";
+'blue'
+turnGreen.style.width = "blue";
+'blue'
+turnGreen.style.width = "50%";
+'50%'
+turnGreen.style.padding = "15px";
+'15px'
+turnGreen.innerText = "Five" 
+'Five'
+
+document.querySelectorAll('li');
+
+// NodeList(3) [li, li, li]
+
+const tempLiList = document.querySelectorAll('li');
+
+// undefined
+
+tempLiList
+// NodeList(3) [li, li, li]
+
+const arr = [1,2,3,4];
+// undefined
+
+tempLiList.style.background="blue";
+// error 
+
+tempLiList[0].style.background="blue";
+'blue'
+
+const myh1 = document.querySelectorAll("h1");
+// undefined
+
+myh1
+
+// NodeList [h1#title.heading]
+
+
+myh1[0].style.background = "green";
+// 'green'
+
+const tempLiList = document.querySelectorAll('li');
+// undefined
+
+tempLiList
+
+// NodeList(3) [li, li, li]0: li1: li2: lilength: 3[[Prototype]]: NodeListentries: ƒ entries()forEach: ƒ forEach()item: ƒ item()keys: ƒ keys()length: (...)values: ƒ values()constructor: ƒ NodeList()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.toStringTag): "NodeList"get length: ƒ length()[[Prototype]]: Object
+
+tempLiList.forEach(function (l) {
+    l.style.color="green";
+})
+
+// undefined
+
+tempLiList.forEach(function nigga(li) {
+    li.style.color="blue";
+    li.style.backgroundColor="Green"
+})
+
+// undefined
